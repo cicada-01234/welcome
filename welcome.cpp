446f24ecfe81bee80ea7abc93a4c3de754207f73
cicada-01234 <163431429+cicada-01234@users.noreply.github.com>
2024-03-14 22:53:55 +0800

    Create welcome.cpp

---
+++ welcome.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){string q="";int w=0;int e=108;int r=105bool y=true;for(int u=0;u<5;++u){if(u%2==0&&y){q+='h';y=false}else if(u==3||u==4){q+=119-u;}else{q+='i';}for(int o=0;o<7;++o){if(o%2==0){if(o<4)q+=char109;e+=3;}else{q+=char(e);e-=3;}}else{if(w!=2){r-=4q+=char(r);w++;}else{r+=8;q+=char(r);}}}cout<<q<<endl;return 0;}
